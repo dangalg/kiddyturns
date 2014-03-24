42e54603b2f3cfac6c7bb8b3b627e16e5dc8a8eb
dangalg <dangalg@gmail.com>
2014-03-24 06:34:14 +0200

    first commit prohect has begun!

---
+++ src/MainActivity.java
@@ -0,0 +1,37 @@
+
+import com.dangal.kiddyturns.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	public void onOptionsMenuClosed(Menu menu) {
+		// TODO Auto-generated method stub
+		super.onOptionsMenuClosed(menu);
+	}
+
+	
+}
